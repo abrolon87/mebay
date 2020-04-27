863b0d4106e268b96769f42bb4605dffea8580c5
Amanda <main@Amandas-MacBook-Air.local>
2020-04-27 19:33:59 -0400

    setting up views

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,2 +1,19 @@
 class AdsController < ApplicationController
+
+  def show 
+    @ad = Ad.find(params[:id])
+  end 
+
+  def index
+    @ads = Ad.all
+  end
+
+  def new 
+    @ad = Ad.new
+  end
+
+  def create
+    @ad = Ad.create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/ads/new', :controller=>'ads', :action=>'new'
+  post '/ads/create', :controller=>'ads', :action=>'create'
+  get '/ads/', :controller=>'ads', :action=>'index'
   get '/ads/:id', :controller=>'ads', :action=>'show'
   
   # these are default routes that we won't be using
