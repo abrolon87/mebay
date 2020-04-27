594d54cae99749726922df2fcb9d003807295e37
Amanda <main@Amandas-MacBook-Air.local>
2020-04-27 01:08:02 -0400

    added show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/ads/:id', :controller=>'ads', :action=>'show'
+  
+  # these are default routes that we won't be using
+  #get ':controller/:action/:id'
+  #get ':controller/:action/:id.:format'
 end
